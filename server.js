8eae974f9e1c576ca35904f68e9354116083a497
Daniel Stallworth <danielstallworthmit@gmail.com>
2014-08-17 20:00:27 -0500

    Have to use app, not router here, maybe for diff in dependency

--- server.js
+++ server.js
@@ -29,19 +29,17 @@
 
 	// routes ======================================================================
 
-var router = express.Router(); 				// get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-	// do logging
-	console.log('Something is happening.');
-	next(); // make sure we go to the next routes and don't stop here
-});
+	// middleware to use for all requests
+	app.use(function(req, res, next) {
+		// do logging
+		console.log('Something is happening.');
+		next(); // make sure we go to the next routes and don't stop here
+	});
 
 	// api ---------------------------------------------------------------------
 
 	//Welcome Message going to /api
-	router.get('/', function(req, res) {
+	app.get('/api', function(req, res) {
 		res.json({ message: 'hooray! welcome to our api!' });	
 	});
 
